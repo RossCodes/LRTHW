6516e48290270071a6526b0c29d27dac9c0156e2
RossCodes <rossarshad84@gmail.com>
2016-03-05 10:37:47 +0000

    Commit ex3/

---
+++ ex3/ex3_calculate.rb
@@ -0,0 +1,8 @@
+# LRTHW STudy Drill
+
+# Calculating how many days I have until my Makers Academy course Begins:
+
+puts "How many days do I have until my Makers Academy course begins on June 6th?"
+
+puts "Months #{1 + 1}"
+puts "Days #{6 + 31 + 30 + 31 + 6}"
