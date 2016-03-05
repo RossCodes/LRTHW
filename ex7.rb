8c3b344f019bb4f3c0c4195ac7086740cb14fd16
RossCodes <rossarshad84@gmail.com>
2016-03-05 10:45:51 +0000

    Commit ex6.rb - ex9.rb

---
+++ ex7.rb
@@ -0,0 +1,30 @@
+# Exercise 7
+
+# prints string
+puts "Mary had a little lamb."
+# prints string with embedded string 'snow'
+puts "Its fleece was white as #{'snow'}."
+# prints string
+puts "And everywhere that Mary went."
+# Prints ten dots (The string ten times)
+puts "." * 10 # what'd that do? 
+
+# These are all variables with assigned values. 
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r" 
+
+# watch that print vs. puts on this line what's it do? 
+# It keeps them both on the same line.  If they were both puts, they'd be on two lines.
+# If they were both print, there'd be no gap at the end. 
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
